9f446063e9ea2bd04ea39de97e7cd051e9df4390
Maria <maria.senina15@gmail.com>
2023-01-06 12:25:05 -0500

    Create and Manage Objects: Dep. Injection Types

---
+++ spring-level-2/src/main/java/com/senina/maria/springlevel2/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,68 @@
+package com.senina.maria.springlevel2.examples.a1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class YourBusinessClass {
+
+    // @Autowired
+    Dependency1 dependency1;
+
+    // @Autowired
+    Dependency2 dependency2;
+
+    // @Autowired (-> optional)
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("constructor injection");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    // @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("setter injection for dep1");
+        this.dependency1 = dependency1;
+    }
+
+    // @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("setter injection for dep2");
+        this.dependency2 = dependency2;
+    }
+
+    @Override
+    public String toString() {
+        return "YourBusinessClass{" +
+                "dependency1=" + dependency1 +
+                ", dependency2=" + dependency2 +
+                '}';
+    }
+}
+
+@Component
+class Dependency1 {
+
+}
+
+@Component
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+public class DepInjectionLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean(YourBusinessClass.class));
+        }
+    }
+}

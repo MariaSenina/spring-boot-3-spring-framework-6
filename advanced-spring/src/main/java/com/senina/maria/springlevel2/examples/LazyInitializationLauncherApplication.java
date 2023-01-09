40de931418514daab2133074c0f7f299908cb6a0
Maria <maria.senina15@gmail.com>
2023-01-09 17:47:39 -0500

    Advanced Spring: Lazy Initialization (+project setup)

---
+++ advanced-spring/src/main/java/com/senina/maria/springlevel2/examples/LazyInitializationLauncherApplication.java
@@ -0,0 +1,45 @@
+package com.senina.maria.springlevel2.examples;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class ClassA {
+
+}
+
+@Component
+@Lazy // makes it so that the class is NOT initialized at startup
+class ClassB {
+    private ClassA classA;
+
+    @Autowired
+    public ClassB(ClassA classA) {
+        System.out.println("Some initialization logic");
+        this.classA = classA;
+    }
+
+    public void doSomething() {
+        System.out.println("Doing something");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            System.out.println("Initialization of the context is completed");
+            context.getBean(ClassB.class).doSomething();
+        }
+    }
+}

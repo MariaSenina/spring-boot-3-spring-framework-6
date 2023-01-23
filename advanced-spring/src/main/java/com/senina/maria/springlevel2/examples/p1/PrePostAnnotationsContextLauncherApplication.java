28a4b03de43e241e9786194dfa7f124f7f0edc20
Maria <maria.senina15@gmail.com>
2023-01-23 13:11:22 -0500

    Advanced Spring: @PostConstruct and @PreDestroy

---
+++ advanced-spring/src/main/java/com/senina/maria/springlevel2/examples/p1/PrePostAnnotationsContextLauncherApplication.java
@@ -0,0 +1,48 @@
+package com.senina.maria.springlevel2.examples.p1;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class SomeClass {
+    private SomeDependency dependency;
+
+    public SomeClass(SomeDependency dependency) {
+        this.dependency = dependency;
+        System.out.println("All dependencies are ready!");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        dependency.getReady();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("Cleanup");
+    }
+}
+
+@Component
+class SomeDependency {
+    public void getReady() {
+        System.out.println("Logic using SomeDependency");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PrePostAnnotationsContextLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(PrePostAnnotationsContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
